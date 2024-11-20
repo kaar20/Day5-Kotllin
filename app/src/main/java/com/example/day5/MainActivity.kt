cadb33bae42ac2c8fe52691bcc76e694e9f5e3ce
kaar20 <codewithkar@gmail.com>
2024-11-20 13:46:14 +0300

    Finished for the Project

--- app/src/main/java/com/example/day5/MainActivity.kt
+++ app/src/main/java/com/example/day5/MainActivity.kt
@@ -23,12 +23,21 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.day5.ui.theme.Day5Theme
+import java.time.format.TextStyle
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +58,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConvertor(){
+    var inputValue by remember{ mutableStateOf("") }
+    var outputValue by remember{mutableStateOf("")}
+    var inputUnit by remember{ mutableStateOf("Meters")}
+    var outputUnit by remember { mutableStateOf("Meters")}
+    var isExpanded by remember{ mutableStateOf(false) }
+    var oExpanded by remember {  mutableStateOf(false) }
+    val conversionFactor = remember { mutableStateOf(1.00) }
+    val oconversionFactor = remember { mutableStateOf(1.00) }
+
+
+
+    fun ConvertUnits(){
+        val inputValueDouble = inputValue.toDoubleOrNull()?:0.0
+        val result = (inputValueDouble*conversionFactor.value*100.0/oconversionFactor.value).roundToInt()/100.0
+        outputValue = result.toString()
+    }
+
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -57,27 +84,48 @@ fun UnitConvertor(){
     ) {
 //        Greeting(name = "Mohamed Abdiaziz")
 //        Greeting(name = "Omar Abdiaziz"
-        Text("Unit Convertor")
+        Text("Unit Convertor",
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 20.sp,
+                color = Color.DarkGray,
+                fontFamily = FontFamily.Monospace
+            )
+        )
         Spacer(modifier= Modifier.height(16.dp))
-        OutlinedTextField(value = "Enter a Unit", onValueChange = {
+        OutlinedTextField(value = inputValue, onValueChange = {
+            inputValue= it
 
-        } )
+        },
+            label = { Text(text = "Enter Value")})
         Spacer(modifier= Modifier.height(16.dp))
 
 
         Row {
       Box{
-          Button(onClick = { /*TODO*/ }) {
+          Button(onClick = {isExpanded=true }) {
 
-              Text("Select")
+              Text(inputUnit)
               Icon(Icons.Default.ArrowDropDown,
                   contentDescription = "")
           }
-          DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
-              DropdownMenuItem(text = {Text("Mohamed") }, onClick = { /*TODO*/ })
-              DropdownMenuItem(text = {Text("Guled") }, onClick = { /*TODO*/ })
+          DropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded=false }) {
+              DropdownMenuItem(text = {Text("Centimeters") }, onClick = {
+                  isExpanded = false
+                  inputUnit="Centimeters"
+                  conversionFactor.value=0.01
+                  ConvertUnits()
+              })
+              DropdownMenuItem(text = {Text("Meters") }, onClick = {
+                  isExpanded = false
+                  inputUnit="Meters"
+                  conversionFactor.value=1.0
+                  ConvertUnits()
+              })
 
-              DropdownMenuItem(text = {Text("Faarah") }, onClick = { /*TODO*/ })
+              DropdownMenuItem(text = {Text("Feet") }, onClick = {  isExpanded = false
+                  inputUnit="Feet"
+                  conversionFactor.value=0.3048
+                  ConvertUnits() })
 
 
           }
@@ -85,17 +133,30 @@ fun UnitConvertor(){
       }
             Spacer(modifier = Modifier.width(16.dp))
         Box{
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { oExpanded=true }) {
 
-                Text("Select")
+                Text(outputUnit)
                 Icon(Icons.Default.ArrowDropDown,
                     contentDescription = "")
             }
-            DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
-                DropdownMenuItem(text = {Text("Mohamed") }, onClick = { /*TODO*/ })
-                DropdownMenuItem(text = {Text("Guled") }, onClick = { /*TODO*/ })
+            DropdownMenu(expanded = oExpanded, onDismissRequest = { oExpanded=false }) {
+                DropdownMenuItem(text = {Text("Centimeters") }, onClick = {
+                    oExpanded = false
+                    outputUnit="Centimeters"
+                    oconversionFactor.value=0.01
+                    ConvertUnits()
+                })
+                DropdownMenuItem(text = {Text("Meters") }, onClick = {
+                    oExpanded = false
+                    outputUnit="Meters"
+                   oconversionFactor.value=1.0
+                    ConvertUnits()
+                })
 
-                DropdownMenuItem(text = {Text("Faarah") }, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = {Text("Feet") }, onClick = {  oExpanded = false
+                    outputUnit="Feet"
+                    oconversionFactor.value=0.3048
+                    ConvertUnits() })
 
 
             }
@@ -104,7 +165,15 @@ fun UnitConvertor(){
     }
         Spacer(modifier= Modifier.height(16.dp))
 
-        Text(text = "Result is : ")
+        Text(text = "Result is : $outputValue $outputUnit",
+
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 20.sp,
+                color = Color.DarkGray,
+                fontFamily = FontFamily.Monospace
+            )
+
+        )
     }
 }
 
